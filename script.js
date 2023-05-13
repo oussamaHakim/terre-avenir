12e9aa784c38c4497382adb36bc36fbf8cc91c9b
Hakim <maycom.hakim@gmail.com>
2023-05-14 00:00:26 +0200

    fin de premier section

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const nav = document.querySelector(".main-nav");
 const navToggler = document.querySelector(".nav-toggler");
 const togglerImg = navToggler.querySelector("img");
 const navLinks = document.querySelectorAll(".main-nav a");
+const logo =  document.querySelector(".logo-head");
+const makeDonation = document.querySelector(".Make-donation");
 
 navToggler.addEventListener("click", toggleNav);
 
@@ -12,12 +14,15 @@ function toggleNav() {
 
   if (togglerImg.src.includes("hamburger")) {
     togglerImg.src = "ressources/pack/cross.svg";
-
+    logo.style.display = "none"
+    makeDonation.style.display ="none"
     /*pour l'accessibilité et donner des indication supplémentation à l'utilisateur*/
 
     navToggler.ariaExpanded = true;
   } else {
     togglerImg.src = "ressources/pack/hamburger.svg";
+    logo.style.display = ""
+    makeDonation.style.display =""
     navToggler.ariaExpanded = false;
   }
 }
